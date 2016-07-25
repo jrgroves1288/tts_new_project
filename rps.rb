ee6ea56ffbd6e56c5cb10dacc813ef1354fdb62d
Robert Brewer <robertdbrewer3@gmail.com>
2016-07-25 11:19:43 -0400

    Updated Rps.rb

---
+++ rps.rb
@@ -0,0 +1,36 @@
+#other ways to do it
+#array = ["rock", "paper", "scissors"]
+# comp = array.sample
+cpucount = 0
+playercount = 0
+tiecount = 0
+ until  (playercount == 5 || cpucount == 5)
+  puts "Chose your Weapon. rock, paper, scissors"
+  array = ["rock", "paper", "scissors"]
+    player1 = gets.chomp
+  cpuplayer = array.sample
+  puts "You thow #{player1}"
+  puts "CPU throws #{cpuplayer}"
+  if (player1 == "rock" && cpuplayer == "scissors") || (player1 == "paper" && cpuplayer == "rock") || (player1 == "scissors" && cpuplayer == "paper")
+    puts "You Win!"
+    playercount +=1
+  elsif (player1 == cpuplayer)
+    puts "You tied!"
+    tiecount +=1
+  else
+    puts "You lose"
+    cpucount +=1
+  end
+
+  puts "You: #{playercount}"
+  puts "CPU: #{cpucount}"
+  puts "Ties: #{tiecount}"
+
+  if playercount == 5
+    puts"You are the Rock, Paper, Scissors Champ!!!"
+  elsif cpuplayer == 5
+    puts "you are the Rock, Paper, Scissors Chump!!!!"
+  else 
+    "Its a draw, nobody wins, Your both Losers"
+  end
+end
